537ceb43833b925702e93f3afd7f0d05aad39445
pro-masud <promasudbd@gmail.com>
2024-09-18 08:01:49 +0600

    feat: portfolio text domain plugin load

--- portfolio-with-load-more/plugin.php
+++ portfolio-with-load-more/plugin.php
@@ -16,4 +16,19 @@
     define('PORTFOLIO_HELPER_DIR', trailingslashit(plugin_dir_path( __FILE__ )));
 
     /** Portfolio includes directory path */ 
-    define("PORTFOLIO_HELPER_INCLUDES_DIR", trailingslashit( plugin_dir_path( __FILE__ ) ));
+    define("PORTFOLIO_HELPER_INCLUDES_DIR", trailingslashit( PORTFOLIO_HELPER_DIR . 'includes' ));
+
+    // create a plugin main Class
+    class PortfolioLoadMore{
+        // create a plugin construct function
+        public function __construct(){
+            add_action("plugin_loaded", array($this, 'Portfolio_Text_Domain_loaded' ));
+        }
+
+        /** Plugin Text Domain Loaded fuction */ 
+        public function Portfolio_Text_Domain_loaded(){
+            load_plugin_textdomain('portfolio', false, trailingslashit( PORTFOLIO_HELPER_DIR . 'language' ) );
+        }
+    }
+
+    new PortfolioLoadMore();
