2b64bc7c0f3341c1d2dd83def970fac503b145fa
pro-masud <promasudbd@gmail.com>
2024-09-18 07:36:10 +0600

    feat: create a new wp plugin portfolio with in load more

---
+++ portfolio-with-load-more/plugin.php
@@ -0,0 +1,9 @@
+/**
+     * Plugin Name: Portfolio with Load More
+     * version: 1.0.0
+     * Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, similique!
+     * Author: MR9
+     * Author URI: promasudbd@gmail.com
+     * Text Domain: mr-9 
+     * Domain Path:/language
+     * */ 
