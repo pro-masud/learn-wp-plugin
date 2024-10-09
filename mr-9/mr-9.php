3a9a92e6ff3e62166b26bf0c642607b70de469c5
pro-masud <promasudbd@gmail.com>
2024-10-09 15:56:28 +0600

    plugin assets management complete

--- mr-9/mr-9.php
+++ mr-9/mr-9.php
@@ -1,58 +1,68 @@
 <?php 
 /**
  * Plugin Name: MR9
- * version: 1.0.0
+ * Version: 1.0.0
  * Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, similique!
  * Author: Masud Rana
  * Author URI: promasudbd@gmail.com
  * Text Domain: mr-9
- * Domain Path:/language
- * */ 
+ * Domain Path: /language
+ */
 
- if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
 }
 
 /**
- * This is a MR-9 Plugin Main Class
- * */ 
+ * This is the MR-9 Plugin Main Class
+ */ 
 
 final class MR_9 {
+    /**
+     * Plugin Version
+     * 
+     * @var string
+     */ 
+    private static $version = '1.0';
 
     /**
      * Class Construct Function Here
-     * 
-     * @return \MR_9
-     * */ 
-    function __construct(){
-
+     */ 
+    function __construct() {
+        $this->define_constants();
     }
 
     /**
-     * Initailizes a singletan instance
+     * Initializes a singleton instance
      * 
-     * @return \MR_9
-    */
-    public static function init(){
+     * @return \MR_9;
+     */
+    public static function init() {
         static $instance = false;
 
-        if(! $instance){
+        if ( ! $instance ) {
             $instance = new self();
         }
 
         return $instance;
     }
+
+    /**
+     * Defines constants used in the plugin
+     */ 
+    public function define_constants() {
+        define('MR_9_VERSION', self::$version);
+    }
 }
 
 /**
  * Initializes the main plugin
- * */ 
-
-function Mr_9(){
+ */ 
+function Mr_9() {
     return MR_9::init();
 }
 
 /**
  * Kick-off the plugin
- * */ 
+ */ 
 Mr_9();
