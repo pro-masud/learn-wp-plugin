22dc6ee74ca8eea29400e577dfca2b7298208136
pro-masud <promasudbd@gmail.com>
2024-10-12 10:47:38 +0600

    feat: addressbook plugin file creation complete

---
+++ mr-9/includes/Admin/Addressbook.php
@@ -0,0 +1,10 @@
+<?php 
+
+namespace Promasud\MR_9\Admin;
+
+class Addressbook {
+
+    public function mr9_plugin_page(){
+        echo "Hello World";
+    }
+}
--- mr-9/includes/Admin/Menu.php
+++ mr-9/includes/Admin/Menu.php
@@ -13,16 +13,19 @@ class Menu {
 
     public function admin_menu(){
         $parent_slug = 'mr-9';
-        $capabality = 'manage_options';
-        add_menu_page( __('MR9', 'mr-9'), __('MR9', 'mr-9'), $capabality , $parent_slug, [$this, 'mr9_plugin_page'], 'dashicons-welcome-learn-more' );
-        add_submenu_page($parent_slug, __('Address Book', 'mr-9'), __('Address Book', 'mr-9'),$capabality, 'mr-9-address-book', [$this, 'mr9_address_book'] );
-    }
-
-    public function mr9_plugin_page(){
-        echo "Hello World";
+        $capability = 'manage_options';
+        add_menu_page( __('MR9', 'mr-9'), __('MR9', 'mr-9'), $capability , $parent_slug, [$this, 'mr9_address_book'], 'dashicons-welcome-learn-more' );
+        add_submenu_page($parent_slug, __('Address Book', 'mr-9'), __('Address Book', 'mr-9'),$capability, $parent_slug, [$this, 'mr9_address_book'] );
+        add_submenu_page($parent_slug, __('Settings', 'mr-9'), __('Settings', 'mr-9'),$capability, 'mr9-settings', [$this, 'mr9_address_book_settings'] );
     }
 
     public function mr9_address_book(){
-        echo 'This is a Address Book';
+        $addressbook = new Addressbook();
+
+        $addressbook -> mr9_plugin_page();
     }
-} 
+
+    public function mr9_address_book_settings(){
+        echo 'This is a Address Book Settings';
+    }
+}
