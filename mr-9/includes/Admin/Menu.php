3cc5b1e69ec74f76a91a1cfdaa9bbd8dd42d7f33
pro-masud <promasudbd@gmail.com>
2024-10-12 10:10:07 +0600

    feat: sub menu creation complete

--- mr-9/includes/Admin/Menu.php
+++ mr-9/includes/Admin/Menu.php
@@ -12,10 +12,17 @@ class Menu {
     }
 
     public function admin_menu(){
-        add_menu_page(__('MR9', 'mr-9'), __('MR9', 'mr-9'), 'manage_options', 'mr-9', [$this, 'mr9_plugin_page'], 'dashicons-welcome-learn-more' );
+        $parent_slug = 'mr-9';
+        $capabality = 'manage_options';
+        add_menu_page( __('MR9', 'mr-9'), __('MR9', 'mr-9'), $capabality , $parent_slug, [$this, 'mr9_plugin_page'], 'dashicons-welcome-learn-more' );
+        add_submenu_page($parent_slug, __('Address Book', 'mr-9'), __('Address Book', 'mr-9'),$capabality, 'mr-9-address-book', [$this, 'mr9_address_book'] );
     }
 
     public function mr9_plugin_page(){
         echo "Hello World";
+    }
+
+    public function mr9_address_book(){
+        echo 'This is a Address Book';
     }
 } 
