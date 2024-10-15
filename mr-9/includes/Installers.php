1dbaa552e01b8cc534ed12edfad91f8c55b14e30
pro-masud <promasudbd@gmail.com>
2024-10-15 19:27:31 +0600

    feat: now plugin create a database table creation successfully complete

--- mr-9/includes/Installers.php
+++ mr-9/includes/Installers.php
@@ -10,7 +10,6 @@ class Installers{
         
     }
 
-
     public function add_version(){
         $installed = get_option('mr_9_installed');
 
@@ -18,10 +17,29 @@ class Installers{
             update_option('mr_9_installed', time());
         }
 
-        update_option('mr_9_version', MR_9_VERSION); // Corrected here
+        update_option('mr_9_version', MR_9_VERSION);
     }
 
     public function create_table(){
-      
+        
+        global $wpdb;
+
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $schema = " CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}mr9_addresses` (
+            `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+            `name` varchar(100) NOT NULL DEFAULT '',
+            `address` varchar(255) DEFAULT NULL,
+            `phone` varchar(30) DEFAULT NULL,
+            `create_by` bigint(20) unsigned NOT NULL,
+            `created_at` datetime NOT NULL,
+            PRIMARY KEY (`id`)
+        ) $charset_collate";
+
+        if( ! function_exists( 'dbDelta' )){
+            require_once ABSPATH . 'wp-admin/includes/upgrade.php'; 
+        }
+
+        dbDelta($schema);
     }
 }
