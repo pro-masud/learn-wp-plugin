d97793fbe698390ec64b46fc8ddd9fec143a0454
pro-masud <promasudbd@gmail.com>
2024-10-22 20:39:38 +0600

    feat: post request javascript function creation and intization complete

--- mr-9/assets/js/enquiry.js
+++ mr-9/assets/js/enquiry.js
@@ -2,8 +2,11 @@
     $('#mr9-enuiry-form form').on('submit', function(e){
         e.preventDefault();
 
-        
+        var data = serialize();
 
+        $.post( '', {paran1: 'value1'}, function(data, textStatus, xhr){
+            
+        });
 
     });
 })(jQuery);
