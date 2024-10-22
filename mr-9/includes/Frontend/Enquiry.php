a1935497fa36442e50a8ac435a9951dfd0daec25
pro-masud <promasudbd@gmail.com>
2024-10-22 18:29:13 +0600

    feat: enquiry file and enquiry class creation successfully

---
+++ mr-9/includes/Frontend/Enquiry.php
@@ -0,0 +1,36 @@
+<?php 
+namespace Promasud\MR_9\Frontend;
+
+/**
+ * 
+ * Intailization Class 
+ * 
+ * */ 
+class Enquiry{
+
+    /**
+     * 
+     * Initializes the class
+     * 
+     * */ 
+
+    function __construct(){
+
+    }
+
+    /**
+     * 
+     * Shortcode Handler Class
+     *
+     * @param array $atts
+     * @param string $content
+     * 
+     * @return string
+     *  
+     * */
+    
+    public function render_shortcode( $atts, $content = '' ){
+        
+    }
+
+}
