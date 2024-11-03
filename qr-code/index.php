0ca741d6278e795133ccecbece4c155e82a5a911
pro-masud <promasudbd@gmail.com>
2024-11-03 17:11:21 +0600

    feat: qr code plugin into

---
+++ qr-code/index.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * Plugin Name: QR Code
+ * Version: 1.0.0
+ * Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, similique!
+ * Author: Masud Rana
+ * Author URI: http://promasudbd.com
+ * Text Domain: qr-code
+ * Domain Path: /language
+ */
