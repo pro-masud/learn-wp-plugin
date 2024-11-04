3b3901ca265f199aa1fc9f4e0281a66f8f9e7667
pro-masud <promasudbd@gmail.com>
2024-11-04 22:42:11 +0600

    feat: wp-dark-mode plugin text-domain include successfully

--- wp-dark-mode/wp-dark-mode.php
+++ wp-dark-mode/wp-dark-mode.php
@@ -9,71 +9,72 @@
  * Domain Path: /language
  */
 
-    if ( ! defined( 'ABSPATH' ) ) {
-        exit; // Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+final class WP_Dark_Mode {
+    /**
+     * Plugin Version
+     * 
+     * @var string
+     */ 
+    const version = '1.0';
+
+    /**
+     * Class Construct Function Here
+     */ 
+    function __construct() {
+        $this->define_constants();
+
+        load_plugin_textdomain( '', false, dirname( WP_Dark_Mode_FILE ) . '/language' );
+        
+        add_action('plugins_loaded', [ $this, 'wp_dark_mode_init_plugin' ]);
+        
     }
 
-    require_once __DIR__ . '/vendor/autoload.php';
-
-    final class WP_Dark_Mode {
-        /**
-         * Plugin Version
-         * 
-         * @var string
-         */ 
-        const version = '1.0';
-
-        /**
-         * Class Construct Function Here
-         */ 
-        function __construct() {
-            $this->define_constants();
-            
-            add_action('plugins_loaded', [ $this, 'wp_dark_mode_init_plugin' ]);
-            
-        }
-
-        public function wp_dark_mode_init_plugin(){
-           
-            new Promasud\WpDarkMode\Admin();
-        }
-
-        /**
-         * Initializes a singleton instance
-         * 
-         * @return \WP_Dark_Mode;
-         */
-        public static function init() {
-            static $instance = false;
-
-            if ( ! $instance ) {
-                $instance = new self();
-            }
-
-            return $instance;
-        }
-
-        /**
-         * Defines constants used in the plugin
-         */ 
-        public function define_constants() {
-            define('WP_Dark_Mode_VERSION', self::version);
-            define('WP_Dark_Mode_FILE', __FILE__);
-            define('WP_Dark_Mode_PATH', __DIR__);
-            define('WP_Dark_Mode_URL', plugins_url('', WP_Dark_Mode_FILE));
-            define('WP_Dark_Mode_ASSETS', WP_Dark_Mode_URL . '/assets');
-        }
-
+    public function wp_dark_mode_init_plugin(){
+        
+        new Promasud\WpDarkMode\Admin();
     }
 
     /**
-     * Initializes the main plugin
-     */ 
-    function WP_Dark_Mode() {
-        return WP_Dark_Mode::init();
+     * Initializes a singleton instance
+     * 
+     * @return \WP_Dark_Mode;
+     */
+    public static function init() {
+        static $instance = false;
+
+        if ( ! $instance ) {
+            $instance = new self();
+        }
+
+        return $instance;
     }
 
     /**
-     * Kick-off the plugin
+     * Defines constants used in the plugin
      */ 
-    WP_Dark_Mode();
+    public function define_constants() {
+        define('WP_Dark_Mode_VERSION', self::version);
+        define('WP_Dark_Mode_FILE', __FILE__);
+        define('WP_Dark_Mode_PATH', __DIR__);
+        define('WP_Dark_Mode_URL', plugins_url('', WP_Dark_Mode_FILE));
+        define('WP_Dark_Mode_ASSETS', WP_Dark_Mode_URL . '/assets');
+    }
+}
+
+/**
+ * Initializes the main plugin
+ */ 
+function WP_Dark_Mode() {
+    return WP_Dark_Mode::init();
+}
+
+/**
+ * Kick-off the plugin
+ */ 
+WP_Dark_Mode();
