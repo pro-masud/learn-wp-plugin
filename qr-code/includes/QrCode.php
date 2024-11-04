a2e2e44eb18fe6a2ed32057a8c67cad8a38dd856
pro-masud <promasudbd@gmail.com>
2024-11-04 09:33:07 +0600

    feat: qr code width and height image general option input fields setup complete

--- qr-code/includes/QrCode.php
+++ qr-code/includes/QrCode.php
@@ -4,9 +4,9 @@ namespace ProMausd\QR_Code;
 class QrCode {
      
     function __construct(){
-        add_filter( 'the_content', [$this, 'Qr_Code_Content'] );
+        add_filter( 'the_content', [ $this, 'Qr_Code_Content' ] );
 
-        add_action( 'admin_init', 'qr_code_settings_options' );
+        add_action('admin_init', [ $this, 'qr_code_settings_options' ]);
     }
 
 
@@ -34,32 +34,43 @@ class QrCode {
         return $content;
     }
 
-    public function qr_code_settings_options(){
+    public function qr_code_settings_options() {
         add_settings_field(
             'qr_code_setting_width_id',
             __( 'Qr Code Width', 'qr-code' ),
-            'qr_width_settings_callback',
-            'general',
+            array( $this, 'qr_width_settings_callback' ), // Use array callback with $this
+            'general'
         );
-
+    
         add_settings_field(
             'qr_code_setting_height_id',
             __( 'Qr Code Height', 'qr-code' ),
-            'qr_height_settings_callback',
-            'general',
+            array( $this, 'qr_height_settings_callback' ), // Use array callback with $this
+            'general'
         );
-
+    
         register_setting( 'general', 'qr_code_setting_width_id', array( 
             'sanitize_callback' => 'esc_attr'
         ) );
-
+    
         register_setting( 'general', 'qr_code_setting_height_id', array( 
             'sanitize_callback' => 'esc_attr'
         ) );
     }
-
+    
 
     public function qr_width_settings_callback( $args ){
-
+        $width = get_option( 'qr_code_setting_width_id' );
+    
+        // Correct the id and name attributes to match the width setting.
+        printf( "<input type='text' id='%s' name='%s' value='%s' />", 'qr_code_setting_width_id', 'qr_code_setting_width_id', esc_attr( $width ) );
     }
+    
+    public function qr_height_settings_callback( $args ){
+        $height = get_option( 'qr_code_setting_height_id' );
+    
+        // Use the height setting correctly here.
+        printf( "<input type='text' id='%s' name='%s' value='%s' />", 'qr_code_setting_height_id', 'qr_code_setting_height_id', esc_attr( $height ) );
+    }
+    
 }
