35f8032b1b7b1ac61e9582719177744781cd5c57
pro-masud <promasudbd@gmail.com>
2024-11-05 09:21:03 +0600

    feat: assets file regitster and create a method successfully

--- wp-dark-mode/includes/Assets.php
+++ wp-dark-mode/includes/Assets.php
@@ -7,11 +7,40 @@ class Assets{
         add_action( 'wp_enqueue_scripts', [ $this, 'wp_dark_mode_front_assets_file' ] );
     }
 
-    public function get_assets_styles_file(){
-        
+    public function wp_dark_mode_style_assets_file(){
+        return [
+            'dark-mode-style' => [
+                'src'       => MR_9_ASSETS . '/css/blockout.css',
+                'version'   => filemtime( MR_9_PATH . '/assets/css/blockout.css' ),
+            ],
+        ];
     }
 
-    public function wp_dark_mode_front_assets_file(){
+    public function wp_dark_mode_js_assets_file(){
+        return [
+            'dark-mode' => [
+                'src'       => MR_9_ASSETS . '/js/darkmode-js.min.js',
+                'version'   => filemtime( MR_9_PATH . '/assets/js/darkmode-js.min.js' ),
+                'deps'       => [ 'jquery' ]
+            ]
+        ];
+    }
 
+    public function dark_mode_enqueue_assets_file(){
+        $style_files    = $this->wp_dark_mode_style_assets_file();
+
+
+        foreach( $style_files as $handle => $style){
+            $deps = isset( $style['deps']) ? $style['deps'] : false ;
+
+            wp_register_style( $handle, $style['src'], $deps, $style['version'] );
+        }
+
+        $script_files   = $this->wp_dark_mode_js_assets_file();
+        foreach( $script_files as $handle => $script){
+            $deps = isset( $script['deps']) ? $script['deps'] : false ;
+
+            wp_register_script( $handle, $script['src'], $deps, $script['version'], true );
+        }
     }
 }
