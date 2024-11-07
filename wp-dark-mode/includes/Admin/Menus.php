bfc2c17056505d7449be0c00e965ac627ba0d04a
pro-masud <promasudbd@gmail.com>
2024-11-07 18:26:29 +0600

    feat: wp-dark-mode plugin extra plugin add successfully

--- wp-dark-mode/includes/Admin/Menus.php
+++ wp-dark-mode/includes/Admin/Menus.php
@@ -134,6 +134,53 @@ class Menus {
             'wp-dark-mode-admin-section-page',
             'wp_dark_mode_widget_settings_section',
         );
+
+        add_settings_field(
+            'wp_dark_mode_button_size_settings',
+            __( 'Button Size', 'wp-dark-mode' ),
+            [ $this, 'wp_dark_mode_button_size_setting_callback' ],
+            'wp-dark-mode-admin-section-page',
+            'wp_dark_mode_widget_settings_section',
+        );
+
+        add_settings_field(
+            'wp_dark_mode_icon_size_settings',
+            __( 'Icon Size', 'wp-dark-mode' ),
+            [ $this, 'wp_dark_mode_icon_size_setting_callback' ],
+            'wp-dark-mode-admin-section-page',
+            'wp_dark_mode_widget_settings_section',
+        );
+
+        add_settings_section(
+            'wp_dark_mode_extra_settings_section',
+            'Extra Settings',
+            [ $this, 'wp_dark_mode_print_extra_section_info' ],
+            'wp-dark-mode-admin-section-page',
+        );
+
+        add_settings_field(
+            'wp_dark_mode_wtcc_section',
+            __( 'Want to create a cookie?', 'wp-dark-mode' ),
+            [ $this, 'wp_dark_mode_wtcc_settings_callback' ],
+            'wp-dark-mode-admin-section-page',
+            'wp_dark_mode_extra_settings_section',
+        );
+
+        add_settings_field(
+            'wp_dark_mode_wtmosm_section',
+            __( 'Want to match the OS mode?', 'wp-dark-mode' ),
+            [ $this, 'wp_dark_mode_wtmosm_settings_callback' ],
+            'wp-dark-mode-admin-section-page',
+            'wp_dark_mode_extra_settings_section',
+        );
+
+        add_settings_field(
+            'wp_dark_mode_wyotwb_section',
+            __( 'Want to use your own toggle widget or button?', 'wp-dark-mode' ),
+            [ $this, 'wp_dark_mode_wyotwb_settings_callback' ],
+            'wp-dark-mode-admin-section-page',
+            'wp_dark_mode_extra_settings_section',
+        );
     }
 
     public function wp_dark_mode_print_main_section_info(){
@@ -145,6 +192,13 @@ class Menus {
 
     public function wp_dark_mode_print_widget_settings_section_info(){
         echo esc_html( "Enter Your Setting Options:" );
+    }
+    public function wp_dark_mode_print_extra_section_info(){
+        ?>
+           <p><?php echo esc_html( "The cookies will allow the plugin to keep the dark mode active if the suer enabled it previously.:" ); ?></p> 
+           <p><?php echo esc_html( "The match OS will allow the plugin to activate dark mode if the OS or browser are in Dark mode." ); ?></p> 
+           <p><?php echo esc_html( "Want to use your own widget or element as toggler? mark the last checkbox with the label Want to use your own toggle widget or button?. then add the class darkmode-enable to the element that you want to use as toggle." ); ?></p> 
+        <?php
     }
     
 
@@ -196,15 +250,50 @@ class Menus {
 
     public function wp_dark_mode_button_dark_setting_callback(){
         printf(
-            '<input type="color" id="wp_dark_mode_button_dark_settings" placeholder="0.3s" name="wp_dark_mode_options[wp_dark_mode_button_dark_settings]" value="%s" />',
+            '<input type="color" id="wp_dark_mode_button_dark_settings" name="wp_dark_mode_options[wp_dark_mode_button_dark_settings]" value="%s" />',
             isset( $this->options[ 'wp_dark_mode_button_dark_settings' ] ) ? esc_attr( $this->options[ 'wp_dark_mode_button_dark_settings' ] ) : ''
         );
     }
 
     public function wp_dark_mode_button_light_setting_callback(){
         printf(
-            '<input type="color" id="wp_dark_mode_button_dark_settings" placeholder="0.3s" name="wp_dark_mode_options[wp_dark_mode_button_light_settings]" value="%s" />',
+            '<input type="color" id="wp_dark_mode_button_dark_settings" name="wp_dark_mode_options[wp_dark_mode_button_light_settings]" value="%s" />',
             isset( $this->options[ 'wp_dark_mode_button_light_settings' ] ) ? esc_attr( $this->options[ 'wp_dark_mode_button_light_settings' ] ) : ''
         );
+    }
+
+    public function wp_dark_mode_button_size_setting_callback(){
+        printf(
+            '<input type="range" min="20" max="100" step="5" id="wp_dark_mode_button_size_settings" name="wp_dark_mode_options[wp_dark_mode_button_size_settings]" value="%s" />',
+            isset( $this->options[ 'wp_dark_mode_button_size_settings' ] ) ? esc_attr( $this->options[ 'wp_dark_mode_button_size_settings' ] ) : ''
+        );
+    }
+
+    public function wp_dark_mode_icon_size_setting_callback(){
+        printf(
+            '<input type="range" min="20" max="100" step="5" id="wp_dark_mode_icon_size_settings" name="wp_dark_mode_options[wp_dark_mode_icon_size_settings]" value="%s" />',
+            isset( $this->options[ 'wp_dark_mode_icon_size_settings' ] ) ? esc_attr( $this->options[ 'wp_dark_mode_icon_size_settings' ] ) : ''
+        );
+    }
+
+    public function wp_dark_mode_wtcc_settings_callback(){
+        printf(
+            '<input type="checkbox" id="wp_dark_mode_wtcc_section" name="wp_dark_mode_options[wp_dark_mode_wtcc_section]" value="1" %s />',
+            checked( 1, isset($this->options['wp_dark_mode_wtcc_section']) ? $this->options['wp_dark_mode_wtcc_section'] : 0, false )
+        );  
+    }
+
+    public function wp_dark_mode_wtmosm_settings_callback(){
+        printf(
+            '<input type="checkbox" id="wp_dark_mode_wtmosm_section" name="wp_dark_mode_options[wp_dark_mode_wtmosm_section]" value="1" %s />',
+            checked( 1, isset($this->options['wp_dark_mode_wtmosm_section']) ? $this->options['wp_dark_mode_wtmosm_section'] : 0, false )
+        );  
+    }
+
+    public function wp_dark_mode_wyotwb_settings_callback(){
+        printf(
+            '<input type="checkbox" id="wp_dark_mode_wyotwb_section" name="wp_dark_mode_options[wp_dark_mode_wyotwb_section]" value="1" %s />',
+            checked( 1, isset($this->options['wp_dark_mode_wyotwb_section']) ? $this->options['wp_dark_mode_wyotwb_section'] : 0, false )
+        );  
     }
 }
