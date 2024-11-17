1c7ff653525b379cd7b16652246fc5d0152c8d8d
pro-masud <promasudbd@gmail.com>
2024-11-17 18:59:49 +0600

    feat: admin plugin file code same change

--- simple-crud/admin/class-simple-crud-admin.php
+++ simple-crud/admin/class-simple-crud-admin.php
@@ -52,9 +52,45 @@ class Simple_Crud_Admin {
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
 
-		$this->simple_crud_admin_files();
+		add_action('admin_menu', [$this, 'simple_crud_admin_display_add']);
 
 	}
+
+	public function simple_crud_admin_display_add() {
+        $capabality = 'manage_options';
+        $slug       = 'simple-crud';
+
+        // Add a Menu
+        add_menu_page(
+            __( 'Simple CRUD', 'simple-crud' ),     // Page title
+            __( 'Simple CRUD', 'simple-crud' ),     // Menu title
+            $capabality,                            // Capability
+            $slug,                                  // Menu slug
+            [$this, 'simple_crud_admin_page'],      // Callback function
+            'dashicons-admin-generic',              // Icon URL or Dashicons class
+            100                                     // Position in the menu
+        );
+
+        // Add a submenu
+        add_submenu_page(
+            $slug,                                  // Parent slug
+            'Student List',                         // Page title
+            'Student List',                         // Menu title
+            $capabality,                            // Capability
+            'simple-crud-add',                      // Submenu slug
+            [$this, 'simple_crud_submenu_page_add'] // Callback function
+        );
+    }
+
+    public function simple_crud_admin_page() {
+        echo '<h1>Simple CRUD Admin Page</h1>';
+        echo '<p>Welcome to the Simple CRUD plugin.</p>';
+    }
+
+    public function simple_crud_submenu_page_add() {
+        echo '<h1>Simple CRUD Submenu Page</h1>';
+        echo '<p>This is the submenu under the Simple CRUD plugin.</p>';
+    }
 
 	/**
 	 * Register the stylesheets for the admin area.
@@ -100,9 +136,5 @@ class Simple_Crud_Admin {
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/simple-crud-admin.js', array( 'jquery' ), $this->version, false );
 
-	}
-
-	public function simple_crud_admin_files(){
-		include SIMPLE_CRUD_PATH . '/admin/partials/simple-crud-admin-display.php';
 	}
 }
