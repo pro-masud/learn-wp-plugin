7eada796ea294b766884b5e726b2f9baaef5c18a
pro-masud <promasudbd@gmail.com>
2024-11-18 16:27:12 +0600

    feat: simple crud project student list

--- simple-crud/admin/views/simple-crud-student-view.php
+++ simple-crud/admin/views/simple-crud-student-view.php
@@ -5,19 +5,24 @@
             <tr>
                 <th>ID</th>
                 <th>Student Name</th>
+                <th>Student ID</th>
                 <th>Email</th>
-                <th>Age</th>
-                <th>GPA</th>
-                <th>Degree</th>
+                <th>Message</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody id="tbody">
             <tr>
                 <td>01</td>
-                <td>01</td>
-                <td>01</td>
-                <td>01</td>
-                <td>01</td>
+                <td>Masud Rana</td>
+                <td>42240101334</td>
+                <td>promasudbd@gmail.com</td>
+                <td>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, architecto?</td>
+                <td>
+                    <a href="">Edite</a>
+                    <a href="">view</a>
+                    <a href="">delete</a>
+                </td>
             </tr>
         </tbody>
     </table>
